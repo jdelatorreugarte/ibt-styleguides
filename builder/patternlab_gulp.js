6be95e26bb3594c72e99693ba42978b9a6ad9677
john <j.ugarte@ibt.com>
2016-01-28 14:08:15 -0500

    ignore files

---
+++ builder/patternlab_gulp.js
@@ -0,0 +1,10 @@
+/* 
+ * patternlab-node - v1.0.0 - 2016 
+ * 
+ * Brian Muenzenmeyer, and the web community.
+ * Licensed under the MIT license. 
+ * 
+ * Many thanks to Brad Frost and Dave Olsen for inspiration, encouragement, and advice. 
+ *
+ */
+
